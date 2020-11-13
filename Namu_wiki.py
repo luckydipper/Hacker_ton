f551445a2a6aa7a6354aec6cc83f3771665cd113
LeeWooil <swcon20wooil@naver.com>
2020-11-14 03:40:54 +0900

    Modify

--- Namu_wiki.py
+++ Namu_wiki.py
@@ -7,10 +7,12 @@ page_py = wiki.page(search)
 if page_py.exists():
 #      print("Page - Summary: %s" % page_py.summary[0:100])
 
-    search_dict = {'tag':"search",
+    search_dict = {'tag': search,
                     "patterns":[search],
                     "responses":[page_py.summary]}
-print(search_dict)
+else :
+    print("이해하지 못했습니다")
+
 
 def toJson(search_dict):
     with open('search.json', 'w', encoding='utf-8') as file :
